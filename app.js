47549f0b15e334c2d85a426e74ffd56ae4a0465c
koebe1 <koe.be.pay@web.de>
2021-11-25 13:40:23 +0100

    core types

---
+++ app.js
@@ -0,0 +1,14 @@
+// CORE TYPES of TS
+/*
+core data types
+number -> all numbers
+string -> all text values
+boolean -> true, false --> no truthy, falsy values
+*/
+function add(n1, n2) {
+    return n1 + n2;
+}
+var num1 = 5;
+var num2 = 2.8;
+var result = add(num1, num2);
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,17 @@
+// CORE TYPES of TS
+/* 
+core data types
+number -> all numbers
+string -> all text values
+boolean -> true, false --> no truthy, falsy values 
+*/
+
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const num1 = 5;
+const num2 = 2.8;
+
+const result = add(num1, num2);
+console.log(result);
