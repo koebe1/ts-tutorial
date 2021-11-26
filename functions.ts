fe4891456905f209b63922b51e5ced2b5eaed6fa
koebe1 <koe.be.pay@web.de>
2021-11-26 14:06:02 +0100

    functions type

--- functions.ts
+++ functions.ts
@@ -1,29 +1,40 @@
 // define return type -> func(arg: type): returnType {}
 // ---> define the type you want to return from the function
-function add(n1, n2) {
-    return n1 + n2;
+
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
+
 // void type -> func(arg: type): void {}
 // ---> no return value
 // ---> gets assigned automatically with when only using side effects
-function printResult(num) {
-    console.log("Result: " + num);
+
+function printResult(num: number): void {
+  console.log("Result: " + num);
 }
 printResult(add(5, 12));
+
 // undefined type
 // ---> no value is returned but return keyword is used
-function returnNothing() {
-    return;
+function returnNothing(): undefined {
+  return;
 }
-var someValue;
+let someValue: undefined;
+
 // function type -> let variable: (a1: type, a2: type) => returnType
 // ---> allow to describe which function is used specifically
-var combineValues;
+let combineValues: (a: number, b: number) => number;
+
 combineValues = add;
+
 console.log(combineValues(8, 8));
-// callback
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
+
+// callbacks get also a function type to describe them
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
 }
-addAndHandle(10, 20, function (num) { return console.log(num); });
+
+addAndHandle(10, 20, (num) => console.log(num));
+
+// unknown type -
---
+++ union-aliases.ts
@@ -0,0 +1,47 @@
+// union type -> func(input: number | string)
+// ---> assign multiple possible types to a parameter
+
+// literal type -> const variable = "hello world"
+// ---> TS sets the type to "hello world" since variable is a constant
+
+// type alias -> type Combined = string | number;
+// ---> defines a custom type that usually stores a union type
+
+type Combined = number | string;
+type ResultDescriptor = "as-number" | "as-text";
+
+function combine(
+  input1: Combined,
+  input2: Combined,
+  resultType: ResultDescriptor
+) {
+  let result: Combined;
+
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultType === "as-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+
+  //   if (resultType === "as-number") {
+  //     // + infront of a variable converts it into a number
+  //     // return parseFloat(result)
+  //     return +result;
+  //   } else {
+  //     return result.toString();
+  //   }
+
+  return result;
+}
+
+const combinedNum = combine(30, 36, "as-number");
+console.log(combinedNum);
+
+const combinedTxt = combine("Bene", "K", "as-text");
+console.log(combinedTxt);
+
+const combinedStr = combine("30", "36", "as-number");
+console.log(combinedStr);
