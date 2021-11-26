7fde80764d4e141fa5dfaee1e4eb47c7f09ccafa
koebe1 <koe.be.pay@web.de>
2021-11-26 11:12:38 +0100

    union type

---
+++ objs-arrays-enums.ts
@@ -0,0 +1,43 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   // tuple
+//   role: [number, string];
+// } = {
+//   name: "bene",
+//   age: 27,
+//   hobbies: ["sports", "cooking"],
+//   role: [2, "author"],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN = 10,
+  // read_only gets assigned the value 11 automatically
+  READ_ONLY,
+  // strings are also possible
+  AUTHOR = "AUTHOR",
+}
+
+let person = {
+  name: "bene",
+  age: 27,
+  hobbies: ["sports", "cooking"],
+  role: Role.ADMIN,
+};
+
+// any takes away all advantages ts gives you
+let fav: any[];
+fav = ["Sports", 1, {}, []];
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("is author");
+}
